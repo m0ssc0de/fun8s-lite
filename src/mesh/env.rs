d3e70650b4fcb70c715a4788cf9ad8931f8372a6
m0ssc0de <hi.paul.q@gmail.com>
2020-07-04 18:03:55 +0800

    offline version

--- src/mesh/env.rs
+++ src/mesh/env.rs
@@ -1,4 +1,10 @@
 use crate::error::Error;
+use packer::Packer;
+use std::fs;
+
+#[derive(Packer)]
+#[packer(source = "files/mesh", prefixed = false)]
+struct MeshFile;
 
 pub struct ENV {}
 
@@ -28,21 +34,23 @@ impl ENV {
         Ok(())
     }
     fn install(&self) -> Result<(), Error> {
+        let _ = run_cmd!("mkdir -p /tmp/tmp-nebula");
+        let files = MeshFile::list();
+        for f in files {
+            let data = MeshFile::get(f).unwrap();
+            println!("file: {}", f);
+            fs::write(format!("/tmp/tmp-nebula/{}", f), data).expect("Unable to write file");
+        }
         if let Err(e) = run_cmd!(
-            mkdir -p /tmp/tmp-nebula/
             mkdir -p /etc/nebula/
 
             cd /tmp/tmp-nebula/
-            wget https://github.com/slackhq/nebula/releases/download/v1.2.0/nebula-linux-amd64.tar.gz
+            // wget https://github.com/slackhq/nebula/releases/download/v1.2.0/nebula-linux-amd64.tar.gz
             tar -zxvf nebula-linux-amd64.tar.gz
             pwd
             cp ./nebula ./nebula-cert /usr/local/bin/
 
-            wget https://github.com/mikefarah/yq/releases/download/3.3.2/yq_linux_amd64
-            mv yq_linux_amd64 /usr/local/bin/yq
-            chmod +x /usr/local/bin/yq
-
-            wget https://raw.githubusercontent.com/slackhq/nebula/master/examples/service_scripts/nebula.service
+            // wget https://raw.githubusercontent.com/slackhq/nebula/master/examples/service_scripts/nebula.service
             cp ./nebula.service /etc/systemd/system/nebula.service
         ) {
             println!("install error {}", e);
